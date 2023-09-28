6fae1440d05fae44b82523cfe0a322fc02556ad6
Luis A Castaneda <lcastaa.aql@gmail.com>
2023-09-28 10:30:02 -0700

    Init Commit

---
+++ src/test/java/aqlabs/app/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package aqlabs.app.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
